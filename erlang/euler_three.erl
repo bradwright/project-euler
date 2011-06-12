89db92da71da56adab0f48d8d7bdc6822749ac04
Bradley Wright <brad@intranation.com>
2011-06-12 23:38:59 +0100

    I understand this Erlang function properly now

--- erlang/euler_three.erl
+++ erlang/euler_three.erl
@@ -3,21 +3,8 @@
 %% The prime factors of 13195 are 5, 7, 13 and 29.
 %% What is the largest prime factor of the number 600851475143 ?
 
-%% We know 1 and X are factors
-factors(X) when X >= 1 ->
-    factors(X, 2, [1, X]).
-
-%% We're at the end
-factors(X, Y, F) when X =:= Y ->
-    lists:sort(F);
-%% when remainder is 0 we have a factor
-factors(X, Y, F) when X rem Y =:= 0 ->
-    factors(X, Y + 1, F ++ [Y]);
-%% not a factor, increment and carry on
-factors(X, Y, F) ->
-    factors(X, Y + 1, F).
-
 % Return a list of the prime factors of N in arbitrary order.
+% http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#Euler.27s_sieve
 factor(N) when is_integer(N), 1 < N ->
     factor(N, 2, []).
 

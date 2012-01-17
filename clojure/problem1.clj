56a2b34af46e53f2f31e4b16521811d35b2723ee
Bradley Wright <brad@intranation.com>
2012-01-17 23:18:00 +0000

    Simplify

--- clojure/problem1.clj
+++ clojure/problem1.clj
@@ -5,13 +5,10 @@
 ;;
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(println (str
-          ;; sum all results
-          (reduce
-           +
-           ;; drop
-           (filter
-            ;; true for anything of % 3 or % 5
-            (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5))))
-            ;; all numbers above 0 and below 1000
-            (range 1 1000)))))
+(reduce +
+        ;; drop
+        (filter
+         ;; true for anything of % 3 or % 5
+         (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5))))
+         ;; all numbers above 0 and below 1000
+         (range 1 1000)))

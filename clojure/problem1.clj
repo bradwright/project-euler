b6225871b61b9b56b06ba54f5daa57d5abcd8f19
Bradley Wright <brad@intranation.com>
2012-04-06 11:32:27 +0100

    clojure: Make problem 1 more idiomatic

--- clojure/problem1.clj
+++ clojure/problem1.clj
@@ -5,10 +5,8 @@
 ;;
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(reduce +
-        ;; drop
-        (filter
-         ;; true for anything of % 3 or % 5
-         (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5))))
-         ;; all numbers above 0 and below 1000
-         (range 1 1000)))
+(defn valid-number? [x]
+  "Number is a multiple of 3 or 5"
+  (or (= 0 (mod x 3)) (= 0 (mod x 5))))
+
+(reduce + (filter valid-number? (range 1 1000)))
